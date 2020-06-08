5176f995a905e9f0c9f4a66504f56c4c039b0dcb
Seth Starr <starrseth@hotmail.com>
2020-06-08 22:47:03 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -2,7 +2,7 @@ def using_concat(array,element)
   array.concat(element)
 end 
 
-def using_insert(array)
-  array.insert(4, "element")
+def using_insert(array,element)
+  array.insert(4, element)
 end
 
