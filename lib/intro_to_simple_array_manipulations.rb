d1742264666afc077d0c9700e433ec9249a15017
Seth Starr <starrseth@hotmail.com>
2020-06-08 22:50:16 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -6,3 +6,17 @@ def using_insert(array,element)
   array.insert(4, element)
 end
 
+def using_uniq(array)
+  array.uniq 
+end
+
+def using_flatten(array)
+  array.flatten
+end
+
+def using_delete(array,"element")
+  array.delete("element")
+end
+
+def using_delete_at(array,4)
+  array.delete_at()
